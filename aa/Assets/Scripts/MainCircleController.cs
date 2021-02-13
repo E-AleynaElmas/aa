7ef860b3d6e84f4ae7bf87f1da49813bc65b33fc
E-AleynaElmas <a.aleynaelmas@gmail.com>
2021-02-13 19:10:39 +0300

    Project files and basic moves added.

---
+++ aa/Assets/Scripts/MainCircleController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCircleController : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject smallCircle;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnSmallCircle();
+        }
+    }
+
+    void SpawnSmallCircle()
+    {
+        Instantiate(smallCircle, transform.position, transform.rotation);
+    }
+}
---
+++ aa/Assets/Scripts/SmallCircleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallCircleController : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    Rigidbody2D physics;
+    bool motionController;
+    void Start()
+    {
+        physics = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!motionController)
+        {
+            physics.MovePosition(physics.position + Vector2.up * speed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "TagSpinningCircle")
+        {
+            transform.SetParent(col.transform);
+            motionController = true;
+        }
+    }
+}
